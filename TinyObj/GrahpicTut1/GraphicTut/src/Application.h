130fd04926c6705a57806d805478e364d1b0f265
Benjamin Moore <benjamin.moore@ad.aie.edu>
2016-05-23 15:21:09 +1000

    Very little changed, removed some functions that werent being used

--- TinyObj/GrahpicTut1/GraphicTut/src/Application.h
+++ TinyObj/GrahpicTut1/GraphicTut/src/Application.h
@@ -1,1 +1,19 @@
 #pragma once
+struct GLFWwindow;
+
+class Application
+{
+public:
+	Application();
+	virtual ~Application();
+
+	virtual bool startup();
+	virtual void shutdown();
+
+	virtual bool update();
+	virtual void draw();
+
+	int window_width;
+	int window_height;
+
+};
